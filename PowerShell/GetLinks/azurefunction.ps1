1e7158b6e48a7a909073367f3135889e77410e83
Jack Binnall <jack.binnall@cps.co.uk>
2019-11-15 13:51:22 +0000

    commit

--- PowerShell/GetLinks/azurefunction.ps1
+++ PowerShell/GetLinks/azurefunction.ps1
@@ -19,7 +19,7 @@ $sitesObj = (Get-PnPListItem -List 'sitesAutomation' -Fields "url" -Connection $
 # itterate around each site from the csv
 foreach($siteUrl in $sitesObj)
 {
-    Write-Host $siteUrl["url"]
+    Write-output $siteUrl["url"]
     # make the connection, get ome site information and create object that contains all the site pages
     $connection = Connect-PnPOnline -Url $siteUrl["url"] -Credentials $cred
     $pnpsite = Get-PnPWeb -Connection $connection
@@ -42,8 +42,8 @@ foreach($siteUrl in $sitesObj)
             # itterate around each regular expression match and write it out into the output csv that is pipe delimited 
             foreach($hrefmatch in $hrefmatches)
             {
-                $internalITCon2 = Connect-PnPOnline -Url "https://m365x630080.sharepoint.com/sites/InternalIT" -Credentials $cred
-                Add-PnPListItem -List "Migration Hyperlink Progress" -Values @{"Title" = "N/A"; "SiteTitle" = $site_title;"PageTitle" = $page_title; "Ref" = $fileref; "Match" = $hrefmatch} -Connection $internalITCon2
+                $internalITCon2 = Connect-PnPOnline -Url $itsite -Credentials $cred
+                $donotuse = Add-PnPListItem -List "Migration Hyperlink Progress" -Values @{"Title" = "N/A"; "SiteTitle" = $site_title;"PageTitle" = $page_title; "Ref" = $fileref; "Match" = $hrefmatch} -Connection $internalITCon2
             }
         }
     }
